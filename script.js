38af3b67ff280f2b18f523ab90a9aaae8811d6b4
Camarederic <camarederic@gmail.com>
2026-04-09 22:15:32 +0300

    added effects to the form for the contact section JS & CSS

--- script.js
+++ script.js
@@ -210,3 +210,27 @@ document.querySelectorAll(".service-btn").forEach((service) => {
   });
 });
 // End of Services
+
+// Contact
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
+
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = `Your ${input.placeholder}`;
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+
+  input.addEventListener("blur", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = "Let's Talk";
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+});
+
+// End of Contact
